223648799943a23a32840b22ad33755d2b5e8139
sahascal <soumen_saha_in@yahoo.com>
2021-05-10 19:16:34 +0530

    updated hellomath.py with more exercises

--- hellomath.py
+++ hellomath.py
@@ -1,7 +1,7 @@
-# print built in math functions
+
 import math
 import random
-
+# print built in math functions
 print(dir(__builtins__))
 print('**********===============================================**********')
 print(dir(math),sep='-')
@@ -18,13 +18,23 @@ print("The first member of tuple: " + tpl[3])
 print('**********-----------------------------------------------**********')
 print("The first k-v pair in dictionary: " + dct['Fname'])
 print('**********-----------------------------------------------**********')
-print('**********-----------------------------------------------**********')
 
+# classes and objects
 a = 30
 b= 'apple'
+print('*-----------------------------------------------------------------*')
 print(a, b)
 print('**********-----------------------------------------------**********')
-print("Type of a and b:" type(a), type(b))
+print("Type of a and b:", type(a), type(b))
 print('**********-----------------------------------------------**********')
 print("Mem location of a and b:", id(a), id(b)) 
 print('**********-----------------------------------------------**********')
+print(isinstance(a, int), isinstance(b, str))
+print('*-----------------------------------------------------------------*')
+
+#bin, oct and hex
+print('*-----------------------------------------------------------------*')
+print("Binary of int-16:",bin(16))
+print("Oct of int-16:", oct(16))
+print("Hex of int-16:", hex(16))
+print('*-----------------------------------------------------------------*')
