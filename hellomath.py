86533d044a7bfdabb8d70c8683c95a7297f9d64b
sahascal <soumen_saha_in@yahoo.com>
2021-05-10 19:25:46 +0530

    updates to hellomath.py

--- hellomath.py
+++ hellomath.py
@@ -12,6 +12,7 @@ tpl = ('Sam', '44', 'BLR', 'WF')
 st = {10,20,99,30,40}
 dct = {'Fname':'Sam', 'Gender':'Male', 'Age': '33'}
 print('**********-----------------------------------------------**********')
+print("container types in Py - set, list, tuple and dict")
 print("The second member of list: " + lst[3])
 print('**********-----------------------------------------------**********')
 print("The first member of tuple: " + tpl[3])
@@ -23,8 +24,8 @@ print('**********-----------------------------------------------**********')
 a = 30
 b= 'apple'
 print('*-----------------------------------------------------------------*')
+print("default classes in Py and objects")
 print(a, b)
-print('**********-----------------------------------------------**********')
 print("Type of a and b:", type(a), type(b))
 print('**********-----------------------------------------------**********')
 print("Mem location of a and b:", id(a), id(b)) 
@@ -34,7 +35,17 @@ print('*-----------------------------------------------------------------*')
 
 #bin, oct and hex
 print('*-----------------------------------------------------------------*')
-print("Binary of int-16:",bin(16))
+print("convert integer to bin, oct and hex")
+print("Binary of int-2:",bin(2))
 print("Oct of int-16:", oct(16))
 print("Hex of int-16:", hex(16))
+print('*-----------------------------------------------------------------*')
+
+#random number operations using random module
+random.seed()
+print('*-----------------------------------------------------------------*')
+print("random operations using random module in Py")
+print(random.random())
+print(random.random())
+print(random.randint(10,50))
 print('*-----------------------------------------------------------------*')
