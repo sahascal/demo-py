73d8f962cb139918f4bd3e57ec364a409c814884
sahascal <soumen_saha_in@yahoo.com>
2021-05-10 20:18:45 +0530

    updated testprogram1.py w-F to C temp conversion program

--- testprogram1.py
+++ testprogram1.py
@@ -24,3 +24,21 @@ print("after swap")
 print("a is:",a, "and b is:", b)
 print("| = = = = = = = = = = = = = = = = = = = |")
 
+#understand trunc, floor and ceil for numbers
+a=-2.8
+print("trunc of -2.8:",math.trunc(a)) 
+print("floor of -2.8:",math.floor(a)) 
+print("ceil of -2.8:",math.ceil(a))
+b=1.5
+print("\n")
+print("trunc of 1.5:",math.trunc(b)) 
+print("floor of 1.5:",math.floor(b)) 
+print("ceil of 1.5:",math.ceil(b))
+
+#temp conversion from F to C
+blrTempF = 78
+k=round(5/9,4); 
+blrTempC=(blrTempF-32)*k #math formula for F to C conversion
+print("\n")
+print("BLR temp in Fahrenheit:", blrTempF, "and in Celcius:", blrTempC)
+
